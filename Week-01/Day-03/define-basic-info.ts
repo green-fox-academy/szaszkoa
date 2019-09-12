4f06909ad9b05801a576833420df1858c912066d
Szaszko <szaszkoa@gmail.com>
2019-09-12 16:51:29 +0200

    define basic info excersize completed

---
+++ Week-01/Day-03/define-basic-info.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as a number
+// Your height in meters as a number
+// Whether you are married or not as a boolean
+
+let myName: string = "Szaszko Andras";
+let myAge: number = 28;
+let myHeight: number = 1.75;
+let isMarried: boolean = false;
+
+console.log(myName, "\n",
+            myAge, "\n",
+            myHeight, "\n",
+            isMarried, "\n",
+    );

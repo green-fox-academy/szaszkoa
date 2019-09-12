a7ba35e5b9a1a2bbaee1aeb37371f54b3f311a4e
Szaszko <szaszkoa@gmail.com>
2019-09-12 16:39:10 +0200

    bmi excersize completed

---
+++ Week-01/Day-03/bmi.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+let massInKg: number = 81.2;
+let heightInM: number = 1.78;
+
+console.log(massInKg /(heightInM**2));
+
+// Print the Body mass index (BMI) based on these values
+// BMI= weight/height^2

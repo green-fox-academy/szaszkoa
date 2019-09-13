9d10330bbac343f5b879fa34cdc7e7bc7435a7ca
Szaszko <szaszkoa@gmail.com>
2019-09-13 18:38:38 +0200

    tried to redo it. i managed somehow

--- Week-01/Day-04/draw-triangle.ts
+++ Week-01/Day-04/draw-triangle.ts
@@ -1,6 +1,5 @@
 'use strict';
 
-let lineCount: number = 8;
 
 // Write a program that draws a triangle like this:
 //
@@ -12,11 +11,11 @@ let lineCount: number = 8;
 // The triangle should have as many lines as lineCount is
 
 
+let lineCount: number = 4;
 let buildingBlock: string = "*";
-let something: string = ""
+let nothing: string = ""
 
-
-for (let i: number = 1; i <= lineCount; i++){  
-    something = something+buildingBlock
-    console.log(something)
+for (let i: number = 0; i < lineCount; i++){
+    nothing = nothing+buildingBlock;
+    console.log(nothing);
 }

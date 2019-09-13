38ba4e99b5c240ef322fef3b5e39dca6b7d67415
Szaszko <szaszkoa@gmail.com>
2019-09-13 09:26:19 +0200

    cuboid excersize finished

---
+++ Week-01/Day-04/cuboid.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+// Write a program that stores 3 sides of a cuboid as variables (floats)
+// The program should write the surface area and volume of the cuboid like:
+//
+// Surface Area: 600
+// Volume: 1000
+
+var x: number = 10
+var y: number = 10
+var z: number = 10
+
+console.log("Attributes of the cubboid:")
+
+console.log("Surface Area: "+(x*y*6));
+console.log("volume: "+(x*y*z));

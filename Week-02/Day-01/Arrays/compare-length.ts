e8c639838425e1a302cdefdc0ae386cf19470db8
Szaszko <szaszkoa@gmail.com>
2019-09-16 11:27:20 +0200

    compare length excersize complete

---
+++ Week-02/Day-01/Arrays/compare-length.ts
@@ -0,0 +1,18 @@
+// -  Create a variable named `firstList`
+//    with the following content: `[1, 2, 3]`
+// -  Create a variable named `secondList`
+//    with the following content: `[4, 5]`
+// -  Log "p2 is longer" to the console if `secondList` has more elements than `firstList`
+
+let firstList: number[] = [1,2,3];
+let secondList: number[] = [4,5];
+
+secondList.push(6)
+secondList.push(7)
+
+if (firstList.length < secondList.length){
+    console.log("p2 is longer")
+} else {
+    console.log("its not longer, fistList has: "+ firstList.length + " elements, secondList has: " + secondList.length)
+}
+

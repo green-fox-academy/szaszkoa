297164e93a5ae2952b74f64293e33163069eacef
Szaszko <szaszkoa@gmail.com>
2019-09-25 13:35:12 +0200

    homework, trees

---
+++ Week-03/Day-03/homework.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+// creating 5 trees
+
+let tree1 = ['birch', 'green', '32', 'male'];
+let tree2 = ['oak', 'blue', '15', 'male'];
+let tree3 = ['pine', 'yellow', '20', 'female'];
+let tree4 = ['elm', 'green', '18', 'female'];
+let tree5 = ['cyprus', 'red', '25', 'male'];
+
+console.log(tree1)

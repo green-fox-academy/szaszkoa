6b8dc857fec51e11d819bd4da39899bdb162c342
Szaszko <szaszkoa@gmail.com>
2019-10-23 09:21:44 +0200

    exercise completed with one for loop. not the most elegant, but gets the job done

---
+++ Week-06/Day-02/Excersize-8/createelement-3.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const kids = [
+  { 'petName': 'Wattled crane', 'owner': 'Bryant' },
+  { 'petName': 'Devil, tasmanian', 'owner': 'Alejandro' },
+  { 'petName': 'Mynah, common', 'owner': 'Nelie' },
+  { 'petName': 'Dolphin, common', 'owner': 'Mariele' },
+  { 'petName': 'Gray duiker', 'owner': 'Maddalena' },
+  { 'petName': 'Crab (unidentified)', 'owner': 'Lucine' },
+  { 'petName': 'Ant (unidentified)', 'owner': 'Lorianna' },
+  { 'petName': 'Bison, american', 'owner': 'Tommie' },
+  { 'petName': 'Yellow mongoose', 'owner': 'Vivyan' },
+  { 'petName': 'Carpet snake', 'owner': 'Veda' },
+  { 'petName': 'Lesser mouse lemur', 'owner': 'Isidor' },
+];
+
+let articleElement = document.getElementById('pets');
+
+for(let i = 0; i<kids.length; i++){
+  let parent = document.createElement('h3');
+  parent.innerHTML = kids[i].owner;
+  let child = document.createElement('p');
+  child.innerHTML = kids[i].petName;
+  articleElement.appendChild(parent);
+  articleElement.appendChild(child);
+}

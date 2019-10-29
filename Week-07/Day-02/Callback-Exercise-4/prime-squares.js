6075939a1d49370e5de916574c5a55ab1989aa95
Szaszko <szaszkoa@gmail.com>
2019-10-29 13:55:12 +0100

    created the primechecker function, now its time to implement the timer

--- Week-07/Day-02/Callback-Exercise-4/prime-squares.js
+++ Week-07/Day-02/Callback-Exercise-4/prime-squares.js
@@ -6,18 +6,36 @@
 //    - the timer should fire every 100ms
 //    - the timer should stop when there are no more elements left to be colored
 
+const isPrime = num => {
+  for(let i = 2; i < num; i++)
+    if(num % i === 0) return false;
+  return num > 1;
+};
+
 let numberGenerator = () => {
   let numOfItems = 100;
   let parent = document.getElementsByTagName('section')[0];
   for (let i = 0; i < numOfItems; i++) {
     let newDiv = document.createElement('div');
-    newDiv.innerText = i+1;  
+    newDiv.innerText = i + 1;
     parent.appendChild(newDiv);
   };
 };
 
 let primeChecker = () => {
-  let numbersArray = getElementsByTagName('div');
+  let numbersArray = document.getElementsByTagName('div');
+  for (let i = 0; i < numbersArray.length; i++) {
+    if (isPrime(numbersArray[i].innerText)){
+      numbersArray[i].setAttribute('class', 'prime')
+    } else {
+      numbersArray[i].setAttribute('class', 'not-prime')
+    };
+  };
 };
 
 numberGenerator();
+
+setInterval(
+  primeChecker(),
+  100
+);

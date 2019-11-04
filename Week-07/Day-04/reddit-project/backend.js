3d365a3e90758c8b8585b06e7d896cd7de9b4f01
Szaszko <szaszkoa@gmail.com>
2019-11-04 10:44:00 +0100

    working on the post endpoint

--- Week-07/Day-04/reddit-project/backend.js
+++ Week-07/Day-04/reddit-project/backend.js
@@ -1,2 +1,44 @@
 'use strict';
 
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+const PORT = 8080;
+const databaseName = 'reddit';
+const tableName = 'posts';
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'newuser',
+  password: 'password',
+  database: databaseName
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error(err);
+    return;
+  } else {
+    console.log('Database successfully connected.');
+  };
+});
+
+app.get('/posts', (req, res) => {
+  connection.query(`SELECT * FROM ${tableName}`, 
+  (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500);
+      res.send();
+      return;
+    };
+    res.status(200).setHeader('Contet-type', 'application/JSON');
+    res.setHeader('Accept', 'application/JSON');
+    res.send(result);
+  });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Backend listening on port number ${PORT}.`);
+});
--- Week-07/Day-04/reddit-project/frontend.js
+++ Week-07/Day-04/reddit-project/frontend.js
@@ -1,2 +1,13 @@
 'use strict';
 
+const xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'http://localhost:8080/posts');
+xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4) {
+        let data = JSON.parse(xhr.responseText);
+        console.log(data)
+    };
+};
+xhr.send();
+

05cb3f7e3dd2f6bbc125f821b4e1387a48ff3bb8
Szaszko <szaszkoa@gmail.com>
2019-11-04 17:30:27 +0100

    managed to set up another endpoint

--- Week-07/Day-04/reddit-project/backend.js
+++ Week-07/Day-04/reddit-project/backend.js
@@ -2,10 +2,15 @@
 
 const express = require('express');
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080;
 const databaseName = 'reddit';
 const tableName = 'posts';
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const jsonParser = bodyParser.json();
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -24,18 +29,55 @@ connection.connect((err) => {
 });
 
 app.get('/posts', (req, res) => {
-  connection.query(`SELECT * FROM ${tableName}`, 
-  (err, result) => {
-    if (err) {
-      console.error(err);
-      res.status(500);
-      res.send();
-      return;
-    };
-    res.status(200).setHeader('Contet-type', 'application/JSON');
-    res.setHeader('Accept', 'application/JSON');
-    res.send(result);
-  });
+  connection.query(`SELECT * FROM ${tableName}`,
+    (err, result) => {
+      if (err) {
+        console.error(JSON.stringify(err));
+        res.status(500);
+        res.send();
+        return;
+      };
+      res.status(200).setHeader('Contet-type', 'application/JSON');
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Accept', 'application/JSON');
+      res.send(JSON.stringify(result));
+    });
+});
+
+app.post('/posts', jsonParser, (req, res) => {
+  let inputTitle = req.body.title;
+  let inputURL = req.body.url;
+  // req.header('Content-type', 'application/json') check for it$!
+  connection.query(`INSERT INTO ${tableName}
+    (title, url)
+    VALUES ('${inputTitle}', '${inputURL}');`
+    , (err, result) => {
+      if (err) {
+        res.status(500);
+        res.send(JSON.stringify(err));
+        return;
+      };
+      connection.query(`SELECT * FROM ${tableName}
+      WHERE id = (SELECT MAX(id) FROM posts);`
+        , (err, result) => {
+          if (err) {
+            res.status(500);
+            res.send(JSON.stringify(err));
+            return;
+          } else {
+            res.status(200).setHeader('Content-type', 'application/JSON');
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.setHeader('Accept', 'application/JSON');
+            res.send(JSON.stringify(result));
+          }
+        })
+    });
+});
+
+app.put('/posts/:id/upvote', (req, res) => {
+  connection.query(` ;`, (err, result) => {
+
+  })
 });
 
 

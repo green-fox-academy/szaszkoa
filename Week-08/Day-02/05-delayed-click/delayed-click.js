fdb5cadfeb680df191f3af908c2bef026e2c6ea0
Szaszko <szaszkoa@gmail.com>
2019-11-05 13:50:54 +0100

    exercise completed

--- Week-08/Day-02/05-delayed-click/delayed-click.js
+++ Week-08/Day-02/05-delayed-click/delayed-click.js
@@ -1,4 +1,12 @@
 'use strict';
 
 let button = document.querySelector('button');
+let textContainer = document.querySelector('div');
 
+button.addEventListener('click', delayedText);
+
+function delayedText() {
+  setTimeout(() => {
+    textContainer.textContent = '2 seconds have ellapsed'
+  }, 2000);
+};

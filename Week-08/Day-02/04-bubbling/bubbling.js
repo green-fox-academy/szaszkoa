7276445452203a7cd82ddab5462dc255c6eec86c
Szaszko <szaszkoa@gmail.com>
2019-11-05 12:10:47 +0100

    completed it one way, will try a different approach

--- Week-08/Day-02/04-bubbling/bubbling.js
+++ Week-08/Day-02/04-bubbling/bubbling.js
@@ -8,3 +8,29 @@
 //    - one for navigation
 //    - one for zooming
 
+let navigation = document.querySelector('nav');
+let image = document.querySelector('.img-inspector');
+
+let positionX = 0;
+let positionY = 0;
+let zoom = 200;
+
+navigation.addEventListener('click', manipulation);
+// navigation.addEventListener('click', zoom)
+
+function manipulation(event) {
+  if (event.path[0].textContent == 'Up') {
+    positionY -= 10;
+  } else if (event.path[0].textContent == 'Down') {
+    positionY += 10;
+  } else if (event.path[0].textContent == 'Left') {
+    positionX -= 10;
+  } else if (event.path[0].textContent == 'Right') {
+    positionX += 10;
+  } else if (event.path[0].textContent == 'Zoom in'){
+    zoom += 20;
+  } else if (event.path[0].textContent == 'Zoom out'){
+    zoom -= 20;
+  }
+  image.setAttribute('style', `background-position-y:${positionY}px; background-position-x:${positionX}px; background-size: ${zoom}%`);
+};

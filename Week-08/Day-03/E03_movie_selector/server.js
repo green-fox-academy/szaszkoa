d5a7178f58e23998196723caf502ccfd585d12e6
Szaszko <szaszkoa@gmail.com>
2019-11-06 15:34:18 +0100

    initial commit before starting to work. boilerplates completed and the skeleton of the code

---
+++ Week-08/Day-03/E03_movie_selector/server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 3000;
+const jsonParser = bodyParser.json();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.get('/' ,(req, res) => {
+  res.sendfile('./public/index.html');
+});
+
+// app.post('/send', jsonParser, (req, res) => {
+//   console.log(req.body)
+// });
+
+app.listen(PORT, () => {
+  console.log(`Listening on port number ${PORT}`);
+});

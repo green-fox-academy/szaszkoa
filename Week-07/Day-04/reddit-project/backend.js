aecedccf8897e4beb035baba3949b859fac652ad
Szaszko <szaszkoa@gmail.com>
2019-11-08 15:42:51 +0100

    added PUT methods and refactored code. Also added protection against SQL injection

--- Week-07/Day-04/reddit-project/backend.js
+++ Week-07/Day-04/reddit-project/backend.js
@@ -3,9 +3,10 @@
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+const env = require('dotenv').config();
 const app = express();
 const PORT = 8080;
-const databaseName = 'reddit';
+const databaseName = process.env.DB_DBNAME;
 const tableName = 'posts';
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,9 +14,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const jsonParser = bodyParser.json();
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'newuser',
-  password: 'password',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
   database: databaseName
 });
 
@@ -28,58 +29,87 @@ connection.connect((err) => {
   };
 });
 
+// get all posts
+
 app.get('/posts', (req, res) => {
-  connection.query(`SELECT * FROM ${tableName}`,
-    (err, result) => {
-      if (err) {
-        console.error(JSON.stringify(err));
-        res.status(500);
-        res.send();
-        return;
-      };
-      res.status(200).setHeader('Contet-type', 'application/JSON');
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Accept', 'application/JSON');
-      res.send(JSON.stringify(result));
+  let query = `SELECT * FROM ${tableName}`;
+  connection.query(query, (err, result) => {
+    if (err) {
+      res.status(500).send(JSON.stringify(err));
+      return;
+    };
+    res.set({
+      'Content-type': 'application/JSON',
+      'Access-Control-Allow-Origin': '*',
+      'Accept': 'application/JSON'
     });
+    res.status(200).send(JSON.stringify(result));
+  });
 });
+
+// post a new post, and send back the full JSON of the last post in the same time
 
 app.post('/posts', jsonParser, (req, res) => {
-  let inputTitle = req.body.title;
-  let inputURL = req.body.url;
+  let inputQuery = `INSERT INTO ${tableName} (title, url) VALUES('${connection.escape(req.body.title)}','${connection.escape(req.body.url)}');`;
+  let outputQuery = `SELECT * FROM ${tableName} WHERE id = (SELECT MAX(id) FROM ${tableName})`;
   // req.header('Content-type', 'application/json') check for it$!
-  connection.query(`INSERT INTO ${tableName}
-    (title, url)
-    VALUES ('${inputTitle}', '${inputURL}');`
-    , (err, result) => {
+  connection.query(inputQuery, (err, result) => {
+    if (err) {
+      return;
+    };
+    connection.query(outputQuery, (err, result) => {
       if (err) {
-        res.status(500);
-        res.send(JSON.stringify(err));
+        res.status(500).send(JSON.stringify(err));
         return;
+      } else {
+        res.set({
+          'Content-type': 'application/JSON',
+          'Access-Control-Allow-Origin': '*',
+          'Accept': 'application/JSON'
+        });
+        res.status(200).send(JSON.stringify(result));
       };
-      connection.query(`SELECT * FROM ${tableName}
-      WHERE id = (SELECT MAX(id) FROM posts);`
-        , (err, result) => {
-          if (err) {
-            res.status(500);
-            res.send(JSON.stringify(err));
-            return;
-          } else {
-            res.status(200).setHeader('Content-type', 'application/JSON');
-            res.setHeader('Access-Control-Allow-Origin', '*');
-            res.setHeader('Accept', 'application/JSON');
-            res.send(JSON.stringify(result));
-          }
-        })
     });
+  });
 });
+
+// upvote post
 
 app.put('/posts/:id/upvote', (req, res) => {
-  connection.query(` ;`, (err, result) => {
-
-  })
+  let query = `UPDATE ${tableName} SET score = score +1 WHERE (id = ${connection.escape(req.params.id)})`;
+  connection.query(query, (err, result) => {
+    if (err) {
+      res.status(500).send(JSON.stringify(err));
+      return;
+    } else {
+      res.set({
+        'Content-type': 'application/JSON',
+        'Access-Control-Allow-Origin': '*',
+        'Accept': 'application/JSON'
+      });
+      res.status(200).send(JSON.stringify(result));
+    };
+  });
 });
 
+// downvote post
+
+app.put('/posts/:id/downvote', (req, res) => {
+  let query = `UPDATE ${tableName} SET score = score -1 WHERE (id= ${connection.escape(req.params.id)})`
+  connection.query(query, (err, result) => {
+    if (err) {
+      res.status(500).send(JSON.stringify(err));
+      return;
+    } else {
+      res.set({
+        'Content-type': 'application/JSON',
+        'Access-Control-Allow-Origin': '*',
+        'Accept': 'application/JSON'
+      });
+      res.status(200).send(JSON.stringify(result));
+    };
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Backend listening on port number ${PORT}.`);

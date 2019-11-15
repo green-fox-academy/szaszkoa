93ed045d20ac2bcd544b91c1e76147d0a080c0bc
Szaszko <szaszkoa@gmail.com>
2019-11-15 09:51:09 +0100

    cleaned up the structure, added some TODOs, and deleted old commented out code

--- Reddit/backend.js
+++ Reddit/backend.js
@@ -3,7 +3,10 @@
 const main = document.querySelector('main');
 const url = 'http://localhost:8080/';
 
+// TODO function targetting the used button and disabling the other. 
 
+
+// TODO function manipulating the score text directly instead of re-generating the DOM
 
 
 // defining reusable DOM manipulator functions
@@ -47,10 +50,12 @@ function putPostsIntoDOM(element) {
 
 function removeAllPosts(uniqueTagname) {
     let mainNode = document.querySelector(uniqueTagname);
-    while(mainNode.firstChild){
+    while (mainNode.firstChild) {
         mainNode.removeChild(mainNode.firstChild);
     };
 };
+
+// GET posts fetch request
 
 function getPosts() {
     fetch(`${url}posts`, { method: 'GET' })
@@ -62,15 +67,12 @@ function getPosts() {
             });
         })
         .catch((error) => {
-            console.table({
-                'Message': 'You fucked it up Son, here\'s how',
-                'Error': error
-            });
+            console.log(error);
         });
 };
 
 function upvote(post_id) {
-    fetch(`${url}posts/${post_id}/upvote`, {method: 'PUT', mode: 'cors'})
+    fetch(`${url}posts/${post_id}/upvote`, { method: 'PUT', mode: 'cors' })
         .catch((error) => {
             console.log(error);
         });
@@ -79,7 +81,7 @@ function upvote(post_id) {
 };
 
 function downvote(post_id) {
-    fetch(`${url}posts/${post_id}/downvote`, {method: 'PUT', mode: 'cors'})
+    fetch(`${url}posts/${post_id}/downvote`, { method: 'PUT', mode: 'cors' })
         .catch((error) => {
             console.log(error);
         });
@@ -87,77 +89,4 @@ function downvote(post_id) {
     getPosts();
 };
 
-// xhr.open('GET', 'http://localhost:8080/posts');
-// xhr.onreadystatechange = () => {
-//     if (xhr.readyState == 4) {
-//         let data = JSON.stringify(xhr.responseText);
-//         console.log(data);
-//         divElement.innerText = data;
-//         body.appendChild(divElement);
-//     };
-// };
-// xhr.send();
-
 getPosts();
-
-// const putFetchData = {
-//     method: 'PUT',
-//     mode: 'cors-with-forced-preflight'
-// }
-
-// let xhr = new XMLHttpRequest();
-// xhr.open('PUT', `http://localhost:8080/posts/${post_id}/upvote`);
-// xhr.setRequestHeader('mode', 'cors');
-// xhr.send();
-
-// function upvote(post_id) {
-//     fetch(`${url}posts/${post_id}/upvote`, putFetchData)
-//         .catch((error) => {
-//             console.log({
-//                 'Message': 'You fucked it up Son, here\'s how',
-//                 'Error': error
-//             });
-//         });
-// }
-
-// function downvote(post_id) {
-//     fetch(`${url}posts/${post_id}/downvote`, putFetchData)
-//         .catch((error) => {
-//             console.log({
-//                 'Message': 'You fucked it up Son, here\'s how',
-//                 'Error': error
-//             });
-//         });
-// };
-
-
-/*
-old XHR method, trying to write Fetch to achieve the same functionality
-
-const xhr = new XMLHttpRequest();
-
-xhr.open('GET', 'http://localhost:8080/posts');
-xhr.onreadystatechange = () => {
-    if (xhr.readyState == 4) {
-        let data = JSON.stringify(xhr.responseText);
-        console.log(data);
-        divElement.innerText = data;
-        body.appendChild(divElement);
-    };
-};
-xhr.send();
-
-*/
-
-// xhr.open('POST', 'http://localhost:8080/posts');
-// xhr.onreadystatechange = () => {
-//     if (xhr.readyState == 4) {
-//         xhr.send(
-//             {
-//                 'title': 'placeholder Title',
-//                 'url': 'placeholder URL'
-//             }
-//         )
-//     }
-// }
-

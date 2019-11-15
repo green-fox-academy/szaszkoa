a4a30acdeaef5f008e4b2d93cbe8acf01517adfa
Szaszko <szaszkoa@gmail.com>
2019-11-15 13:46:48 +0100

    added some comments and reorganised the code and functions into logical sections. Maybe later i will separate them into modules

--- Reddit/backend.js
+++ Reddit/backend.js
@@ -5,18 +5,22 @@ const url = 'http://localhost:8080/';
 
 // TODO function targetting the used button and disabling the other. 
 
+let upvoteNode = document.getElementById('post1score')
 
-// TODO function manipulating the score text directly instead of re-generating the DOM
+console.log(`you selected this: ${upvoteNode}`)
 
+// function buttonColorAndDisable(post_id, voteType){
 
-// defining reusable DOM manipulator functions
+// }
 
+// *** GET POSTS FUNCTIONS ***
+
+// defining reusable DOM manipulator function
 function createNode(element) {
     return document.createElement(element);
 };
 
-// defining the post fetching separately
-
+// function to generate ONE post element, reused in the getPosts function
 function putPostsIntoDOM(element) {
     let upvote = createNode('a');
     upvote.setAttribute('class', 'upvote');
@@ -33,6 +37,7 @@ function putPostsIntoDOM(element) {
     let postTitle = createNode('h2');
     let postURL = createNode('p');
     let votes = createNode('span');
+    votes.setAttribute('id', `post${element.post_id}score`);
     postTitle.innerText = element.title;
     postURL.innerText = element.url;
     votes.innerText = element.score;
@@ -46,22 +51,12 @@ function putPostsIntoDOM(element) {
     main.appendChild(postContainer);
 };
 
-// function to remove all elements of the given DOM node
-
-function removeAllPosts(uniqueTagname) {
-    let mainNode = document.querySelector(uniqueTagname);
-    while (mainNode.firstChild) {
-        mainNode.removeChild(mainNode.firstChild);
-    };
-};
-
 // GET posts fetch request
-
 function getPosts() {
     fetch(`${url}posts`, { method: 'GET' })
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
+            console.log(data)
             data.forEach(element => {
                 putPostsIntoDOM(element);
             });
@@ -71,13 +66,29 @@ function getPosts() {
         });
 };
 
+// *** UP/DOWNVOTE FUNCTIONS ***
+
+// function to target the actual score element and changing it directly
+function voteScoreChange(post_id, voteType) {
+    let score = document.getElementById(`post${post_id}score`);
+    let voteScore = parseInt(score.innerText);
+    console.log(voteScore);
+    if (voteType === 'up') {
+        voteScore += 1;
+        score.innerText = voteScore;
+    } else {
+        voteScore -= 1;
+        score.innerText = voteScore;
+    };
+};
+
+// functions for sending the PUT request and calling the voteScoreChange function
 function upvote(post_id) {
     fetch(`${url}posts/${post_id}/upvote`, { method: 'PUT', mode: 'cors' })
         .catch((error) => {
             console.log(error);
         });
-    removeAllPosts('main');
-    getPosts();
+    voteScoreChange(post_id, 'up');
 };
 
 function downvote(post_id) {
@@ -85,8 +96,17 @@ function downvote(post_id) {
         .catch((error) => {
             console.log(error);
         });
-    removeAllPosts('main');
-    getPosts();
+    voteScoreChange(post_id, 'down');
 };
 
 getPosts();
+
+// *** NOT USED CURRENTLY ***
+
+// function to remove all elements of the given DOM node
+function removeAllPosts(uniqueTagname) {
+    let mainNode = document.querySelector(uniqueTagname);
+    while (mainNode.firstChild) {
+        mainNode.removeChild(mainNode.firstChild);
+    };
+};

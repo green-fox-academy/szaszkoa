c8d0217460d2bb02e0ae28f489d650c047ccc54c
Szaszko <szaszkoa@gmail.com>
2019-11-19 17:12:56 +0100

    spent half day resolving this function. Problem: i needed data back, so using Fetch only worked with using POST method. Then, the form on the loginpage had to be gutted, not to send anything. A loginFunction have been assigned to the LOGIN button. But then, upon sending the username/password in the Fetch body, as JSON, it never arrived to the server side. The problem was, that in the POST Fetch header, no Content-Type was set. After including Content-Type: application/JSON in the fetch header, it worked. After half a day :O :( :'(

---
+++ Reddit/public/modules/loginFunction.mjs
@@ -0,0 +1,33 @@
+'use strict'
+
+import { setSessionStorage } from './sessionStorage.mjs';
+
+const url = 'http://localhost:8080';
+
+const loginFunction = () => {
+  // grabbing the input elements
+  let username = document.getElementById('username');
+  let password = document.getElementById('password');
+
+  let requestJSON = {
+    'username': `${username.value}`,
+    'password': `${password.value}`
+  }
+
+  // sending the login credentials
+  let loginSettings = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/JSON' }, // nota bene: headers need to be sent when sending JSON!
+    body: JSON.stringify(requestJSON),
+    mode: 'cors'
+  };
+
+  fetch(`${url}/validate`, loginSettings)
+    .then((response) => response.json())
+    .then(data => setSessionStorage('username', data[0].username))
+    .catch(error => {
+      console.error(error);
+    });
+};
+
+export { loginFunction };

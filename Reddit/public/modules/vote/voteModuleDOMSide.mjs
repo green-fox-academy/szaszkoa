7614e9788ca908bf11813c70fc570e0aaaa41c7d
Szaszko <szaszkoa@gmail.com>
2019-11-23 23:39:00 +0100

    included 2 more functions (could have been made by one, honestly) to handle modifying existing vote, and cancelling votes

--- Reddit/public/modules/vote/voteModuleDOMSide.mjs
+++ Reddit/public/modules/vote/voteModuleDOMSide.mjs
@@ -7,4 +7,16 @@ const voteScoreChange = (post_id, voteType) => {
   voteType === 'up' ? (score.innerText = voteScore += 1) : (score.innerText = voteScore -= 1);
 };
 
-export { voteScoreChange };
+const voteScoreChangeX2 = (post_id, voteType) => {
+  let score = document.getElementById(`post${post_id}score`);
+  let voteScore = parseInt(score.innerText);
+  voteType === 'up' ? (score.innerText = voteScore += 2) : (score.innerText = voteScore -= 2);
+};
+
+const voteScoreReverse = (post_id, voteType) => {
+  let score = document.getElementById(`post${post_id}score`);
+  let voteScore = parseInt(score.innerText);
+  voteType === 'up' ? (score.innerText = voteScore -= 1) : (score.innerText = voteScore += 1);
+};
+
+export { voteScoreChange, voteScoreChangeX2, voteScoreReverse };

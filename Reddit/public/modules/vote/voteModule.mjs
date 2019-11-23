31f52bfba562060b8566c268c5cfadd3516e1e21
Szaszko <szaszkoa@gmail.com>
2019-11-23 23:34:50 +0100

    reworked vote() function, with possible overload argument, added if/else paths to redirect the request made on the vote() function

--- Reddit/public/modules/vote/voteModule.mjs
+++ Reddit/public/modules/vote/voteModule.mjs
@@ -1,46 +1,20 @@
 'use strict'
 
 import { disableVote } from './disableVoteButtons.mjs'
-import { voteScoreChange } from './voteModuleDOMSIDE.mjs';
+import { firstTimeVote } from './voteForTheFirstTime.mjs';
+import { votedAlready } from './voteIfVotedAlready.mjs';
+import { votedButCancelled } from './votedButCancelled.mjs';
 
-const url = 'http://localhost:8080';
+const vote = (post_id, usersVoteType, voteAction = 'NONE') => {
 
-const vote = (post_id, usersVoteType) => {
-  
   if (sessionStorage.getItem('username')) {
-    let voteType = '';
-      let voteButtons = document.getElementsByClassName(`score id${post_id}`)[0].childNodes;
-      if (usersVoteType == 'up') {
-        voteType = 'UP'
-        voteButtons[0].removeAttribute('onclick');
-        voteButtons[0].setAttribute('class', 'upvoted');
-        voteButtons[2].setAttribute('onclick', `vote(${post_id},"down")`);
-        voteButtons[2].setAttribute('class', 'downvote');
-      } else if (usersVoteType == 'down') {
-        voteType = 'DOWN'
-        voteButtons[2].removeAttribute('onclick');
-        voteButtons[2].setAttribute('class', 'downvoted');
-        voteButtons[0].setAttribute('onclick', `vote(${post_id},"up")`);
-        voteButtons[0].setAttribute('class', 'upvote');
-      };
-
-      const fetchPUTSettings = {
-        method: 'PUT',
-        headers: {'Content-Type': 'application/JSON'},
-        body: JSON.stringify({
-          'vote_id': '',
-          'post_id': `${post_id}`,
-          'user_id': `${sessionStorage.getItem('user_id')}`,
-          'vote_type': `${voteType}`
-        }),
-        mode: 'cors'
-      };
-
-      fetch(`${url}/posts/${post_id}/${usersVoteType}vote`, fetchPUTSettings)
-        .then(voteScoreChange(post_id, usersVoteType))
-        .catch((error) => {
-          console.log(error);
-        });
+    if(voteAction == 'NONE'){
+      firstTimeVote(post_id, usersVoteType);
+    } else if (voteAction == 'MOD'){
+      votedAlready(post_id, usersVoteType);
+    } else if (voteAction == 'DEL'){
+      votedButCancelled(post_id, usersVoteType);
+    };
   } else {
     disableVote();
   };

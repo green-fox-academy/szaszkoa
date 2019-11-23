16647368a916a05513720504861371700a99c1e4
Szaszko <szaszkoa@gmail.com>
2019-11-23 23:40:28 +0100

    added a new function to reset the buttons when a vote is cancelled, and expanded existing function to include the vote() function overloads on the onclick attribute on the HTML element, when the post is voted on already by the user in the past

--- Reddit/public/modules/vote/voteButtonInteraction.mjs
+++ Reddit/public/modules/vote/voteButtonInteraction.mjs
@@ -3,16 +3,24 @@
 const buttonInteraction = (postType, post_id) => {
   let voteButtons = document.getElementsByClassName(`score id${post_id}`)[0].childNodes;
   if(postType == 'up'){
-    voteButtons[0].removeAttribute('onclick');
+    voteButtons[0].setAttribute('onclick', `vote(${post_id},"up","DEL")`);
     voteButtons[0].setAttribute('class', 'upvoted');
-    voteButtons[2].setAttribute('onclick', `vote(${post_id},"down")`);
+    voteButtons[2].setAttribute('onclick', `vote(${post_id},"down","MOD")`);
     voteButtons[2].setAttribute('class', 'downvote');
   } else if (postType == 'down'){
-    voteButtons[2].removeAttribute('onclick');
-    voteButtons[2].setAttribute('class', 'downvoted');
-    voteButtons[0].setAttribute('onclick', `vote(${post_id},"up")`);
+    voteButtons[0].setAttribute('onclick', `vote(${post_id},"up","MOD")`);
     voteButtons[0].setAttribute('class', 'upvote');
+    voteButtons[2].setAttribute('onclick', `vote(${post_id},"down","DEL")`);
+    voteButtons[2].setAttribute('class', 'downvoted');
   };
 };
 
-export { buttonInteraction };
+const buttonReset = (post_id) => {
+  let voteButtons = document.getElementsByClassName(`score id${post_id}`)[0].childNodes;
+  voteButtons[0].setAttribute('onclick', `vote(${post_id},"up")`);
+  voteButtons[0].setAttribute('class', 'upvote');
+  voteButtons[2].setAttribute('onclick', `vote(${post_id},"down")`);
+  voteButtons[2].setAttribute('class', 'downvote');
+};
+
+export { buttonInteraction, buttonReset };
